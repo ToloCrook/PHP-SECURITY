c3905c4d1d46e6baecd173ef51f74a0ecbd2e191
ToloCrook <109980427+ToloCrook@users.noreply.github.com>
2022-10-05 21:37:48 +0200

    Update thanks.php

--- thanks.php
+++ thanks.php
@@ -1,18 +1,37 @@
 <?php
 
+
 $errors= [];
 
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    
-    if (!isset($_POST['user_name']) || trim($_POST['user_name'] === '')) {
-        $errors = "Le prénom est obligatoire";
-    } if (!isset($_POST['user_lastname']) || trim($_POST['user_lastname']) === '') {
-        $errors = "Le nom est obligatoire";
-    } if (!isset($_POST['user_email']) || trim($_POST['user_email']) === '' || $_POST['user_email'] != filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL)) {
-        $errors= "L'e-mail est obligatoire";
-    }
-}
 
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if (!isset($_POST['user_name']) || trim($_POST['user_name'] === '')) {
+        $errors[] = "Le prénom est obligatoire";
+
+    } if (!isset($_POST['user_lastname']) || trim($_POST['user_lastname']) === '') {
+        $errors[] = "Le nom est obligatoire";
+        
+    } if (!isset($_POST['user_email']) || trim($_POST['user_email']) === ''  || !filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL)) {
+        $errors[]= "L'e-mail est obligatoire";
+        
+    } if (!isset($_POST['user_phone']) || trim($_POST['user_email']) === '' ) {
+        $errors[]= "Le numéro de téléphone est obligatoire";
+       
+    } foreach($errors as $error) { 
+        echo $error . "<br>";
+    
+    } if (empty($errors)) {
+         
+            
+            echo "Merci " . $_POST['user_name'] . " " . $_POST['user_lastname'] . " de nous avoir contacté à propos de " . $_POST['theme'] . "." . "<br>" .
+    
+            "Un de nos conseillers vous contactera soit à l’adresse " . $_POST['user_email'] . " ou par téléphone au " . $_POST['user_phone']  . " dans les plus brefs délais pour traiter votre demande :" . "<br>" .
+            
+            $_POST['user_message'];
+    }
+};
 
 
 
@@ -20,12 +39,6 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
 
-
-echo "Merci " . $_POST['user_name'] . " " . $_POST['user_lastname'] . " de nous avoir contacté à propos de " . $_POST['theme'] . "." . "<br>" .
-
-"Un de nos conseillers vous contactera soit à l’adresse " . $_POST['user_email'] . " ou par téléphone au " . $_POST['user_phone']  . " dans les plus brefs délais pour traiter votre demande :" . "<br>" .
-
-$_POST['user_message'];
 
 
 
